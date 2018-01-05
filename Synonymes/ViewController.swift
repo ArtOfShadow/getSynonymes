ea88f13556a5357dfed8abf297d91a6c8305c98b
Shaddow <shaddowart@gmail.com>
2018-01-05 15:22:44 +0300

    Initial Commit

---
+++ Synonymes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Synonymes
+//
+//  Created by  Shadow on 05.01.18.
+//  Copyright © 2018  Shadow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
